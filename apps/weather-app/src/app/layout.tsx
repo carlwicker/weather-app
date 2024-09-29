d43144a4133d4681b1455272f3d889d3680cdf8b
Carl Wicker <carlwicker@gmail.com>
2024-09-29 18:00:18 +0100

    accessibility fixes

--- apps/weather-app/src/app/layout.tsx
+++ apps/weather-app/src/app/layout.tsx
@@ -22,7 +22,12 @@ export default function RootLayout({
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
       </head>
-      <body className={`bg-[#100E1D] ${capriola.className}`}>{children}</body>
+      <body className={`bg-[#100E1D] ${capriola.className}`}>
+        <a href="#main" className="hidden">
+          Skip to main content
+        </a>
+        {children}
+      </body>
     </html>
   );
 }
